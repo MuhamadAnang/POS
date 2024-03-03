15d0255cc6379473bd0d3dc02ef2029f601a1814
Muhamad Anang <ananghusnul10@gmail.com>
2024-03-04 04:51:44 +0700

    QUERY BUILDER

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\PenjualanController;
 use App\Http\Controllers\ProductController;
@@ -36,3 +37,4 @@ Route::get('/penjualan', [PenjualanController::class, 'index']);
 
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
