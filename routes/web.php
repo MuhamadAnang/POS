5aa9f9b9a10290c8f33d2f6c09f6d9ea8cc61aec
Muhamad Anang <ananghusnul10@gmail.com>
2024-04-19 10:27:15 +0700

    JOBSHEET 09 - AUTHENTICATION, MIDDLEWARE

--- routes/web.php
+++ routes/web.php
@@ -1,95 +1,5 @@
 <?php
 
-// use App\Http\Controllers\HomeController;
-// use App\Http\Controllers\KategoriController;
-// use App\Http\Controllers\LevelController;
-// use App\Http\Controllers\PenjualanController;
-// use App\Http\Controllers\POSController;
-// use App\Http\Controllers\ProductController;
-// use App\Http\Controllers\UserController;
-// use App\Http\Controllers\WelcomeController;
-// use App\Models\User;
-// use Illuminate\Support\Facades\Route;
-
-
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-// Route::get('/', [HomeController::class, 'index']); 
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-
-// Route::prefix('/category')->group(function () {
-//     Route::get('/', [ProductController::class, 'index']);
-//     Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
-//     Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
-//     Route::get('/home-care', [ProductController::class, 'homeCare'])->name('home-care');
-//     Route::get('/baby-kid', [ProductController::class, 'babyKid'])->name('baby-kid');
-// });
-
-// // Route::get('/user/{id}/name/{name}',[UserController::class,'index']);
-
-// Route::get('/penjualan', [PenjualanController::class, 'index']);
-
-
-
-// Route::get('/level', [LevelController::class, 'index']);
-// Route::get('/kategori', [KategoriController::class, 'index']);
-// Route::get('/user', [UserController::class, 'index']);
-
-// Route::get('/user/tambah', [UserController::class, 'tambah'])->name('/user/tambah');
-// Route::get('/user/ubah{id}', [UserController::class, 'ubah'])->name('/user/ubah');
-// Route::get('/user/hapus{id}', [UserController::class, 'hapus'])->name('/user/hapus');
-
-// Route::get('/user', [UserController::class, 'index'])->name('/user');
-// Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan'])->name('/user/tambah_simpan');
-// Route::put('/user/ubah_simpan{id}', [UserController::class, 'ubah_simpan'])->name('/user/ubah_simpan');
-
-
-// Route::get('/kategori', [KategoriController::class, 'index']);
-
-// Route::get('/kategori/create', [KategoriController::class, 'create']);
-// Route::post('/kategori', [KategoriController::class, 'store']);
-
-// Route::get('/kategori/edit/{id}', [KategoriController::class, 'edit'])->name('kategori.edit');
-// Route::put('/kategori/{id}', [KategoriController::class, 'update'])->name('kategori.update');
-// Route::get('/kategori/delete/{id}', [KategoriController::class, 'destroy'])->name('kategori.delete');
-
-// Route::get('/formuser', function () {
-//     return view('formuser');
-// });
-
-// Route::get('/formlevel', function () {
-//     return view('formlevel');
-// });
-
-// Route::resource('m_user', POSController::class);
-
-
-
-// Route::group(['prefix' => 'user'], function () {
-//     Route::get('/', [UserController::class, 'index']);          //menampilkan halaman awal user
-//     Route::post('/list',[UserController::class,'list']);        //menampilkan data user dalam bentuk json untuk datatables
-//     Route::get('/create',[UserController::class,'create']);     //menampilkan halaman form tambah user
-//     Route::post('/',[UserController::class,'store']);           //menyimpan data user baru
-//     Route::get('/{id}',[UserController::class,'show']);         //menampilkan detail user
-//     Route::get('/{id}/edit',[UserController::class,'edit']);    //menampilkan halaman form edit user
-//     Route::put('/{id}',[UserController::class,'update']);       //menyimpan perubahan data user
-//     Route::delete('/{id}',[UserController::class,'destroy']);   //menghapus data user
-// });
-
 use App\Http\Controllers\BarangController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
@@ -99,6 +9,9 @@ use App\Http\Controllers\TransaksiController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ManagerController;
 
 Route::get('/',[WelcomeController::class,'index']);
 
@@ -143,6 +56,26 @@ Route::post('/transaksi/list', [TransaksiController::class, 'list']);
 // API
 Route::get('/api/barang/{id}/get', [BarangController::class, 'get']);
 
+
+
+Route::get('login',[AuthController::class,'index'])->name('login');
+Route::get('register',[AuthController::class,'register'])->name('register');
+Route::post('proses_login',[AuthController::class,'proses_login'])->name('proses_login');
+Route::get('logout',[AuthController::class,'logout'])->name('logout');
+Route::post('proses_register',[AuthController::class,'proses_register'])->name('proses_register');
+
+// kita atur juga untuk middleware menggunakan group pada routing
+// di dalamnya terdapat group untuk mengecek kondisi login
+// jika user yang login merupakan admin maka akan diarahkan ke AdminController
+// jika user yang login merupakan manager maka akan diarahkan ke userController
+Route::group(['middleware' => ['auth']], function () {
+    Route::group(['middleware' => 'cek_login:1'], function () {
+        Route::resource('admin', AdminController::class);
+    });
+    Route::group(['middleware' => 'cek_login:2'], function () {
+        Route::resource('manager', ManagerController::class);
+    });
+});
 
 
 
